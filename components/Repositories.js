71a0b32da0c66dd91fd12c8da376b13d6096b513
medson10 <medsonoliveira@outlook.com.br>
2018-01-15 00:26:47 -0200

    Add text to loading and not found result

--- components/Repositories.js
+++ components/Repositories.js
@@ -4,11 +4,12 @@ import styled from 'styled-components/native';
 import { compose, pure } from 'recompose';
 import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
+import Repository from './Repository';
 
 const Container = styled.View`
-  margin-top: 20px;
+  margin-top: 10px;
   width: 320px;
-  height: 320px;
+  height: 400px;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -17,8 +18,28 @@ const Container = styled.View`
 const ItemList = styled.Text`
   margin-top: 10px;
   font-size: 15px;
-  color: #006;
+  color: #333;
   text-align: center;
+`;
+
+const LoadingText = styled.Text`
+  font-size: 18px;
+  background-color: #070;
+  color: #FFF;
+  margin: 70px 30px;
+  padding: 30px 60px;
+  border-radius: 50;
+`;
+
+const ErrorText = styled.Text`
+  font-size: 18px;
+  background-color: #D00;
+  color: #FFF;
+  margin: 70px 30px;
+  padding: 30px 60px;
+  border-radius: 50;
+  text-align: center;
+  line-height: 30px;
 `;
 
 const Repositories = ({data: { loading, repositoryOwner, refetch }}, props) =>
@@ -26,10 +47,17 @@ const Repositories = ({data: { loading, repositoryOwner, refetch }}, props) =>
       {
         !loading && repositoryOwner ?
           <Container>
-            <FlatList keyExtractor={(item, index) => index} data={repositoryOwner.repositories.nodes} renderItem={({item}) => <ItemList>{item.name}</ItemList>}></FlatList>
+            <FlatList keyExtractor={(item, index) => index} data={repositoryOwner.repositories.nodes} renderItem={({item}) => <Repository item={item} />}></FlatList>
           </Container>
           :
-          <Text>{loading ? 'Loading...' : 'Github user not found'}</Text>
+          <View>
+            {
+              loading ?
+                <LoadingText>Searching...</LoadingText>
+                :
+                <ErrorText>Sorry, username not found</ErrorText>
+            }
+          </View>
       }
     </View>
 
